a384ccb956bec66d14dad47e4b8bcca1e9c3ce5d
Vojtěch Lukáš <vlukas@hotmail.cz>
2023-04-22 18:09:20 +0200

    struggless with datetime:((

--- src/backend/Modules/getCsvWeather.ts
+++ src/backend/Modules/getCsvWeather.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 const csvtojson = require('csvtojson');
 
 export async function getCsvWeather(url: string) {
+    let date = new Date();
+    let currDateTime: string;
+    currDateTime = `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}:${date.getHours()}`;
+    url += `;date=${currDateTime}`;
+    //console.log(url);
     try {
       const { data, status } = await axios.get<any>(
         url,
@@ -10,9 +15,7 @@ export async function getCsvWeather(url: string) {
         },
       );
         console.log(data);
-        csvtojson({
-            delimiter: ';'
-        })
+        csvtojson({ delimiter: ';' })
             .fromString(data)
             .then((jsonObj: JSON) => {
                 console.log(jsonObj);
